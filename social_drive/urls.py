60d79902fe3895db423d24bb108d5adf76c01a21
jamshaid <jamshaid1824@gmail.com>
2019-09-14 04:15:58 +0500

    login add

--- social_drive/urls.py
+++ social_drive/urls.py
@@ -13,13 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('google_drive.urls')),
+    url(r'^login/$', auth_views.LoginView.as_view(), {'redirect_authenticated_user': True},
+        name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': 'connect_google_drive'},
+        name='logout'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
